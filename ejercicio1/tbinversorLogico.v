edf73046a130645193b296aa9306a24797ec0cb3
torjaquintero <torjaquintero@gmail.com>
2023-10-23 22:41:18 -0500

    Update tbinversorLogico.v

---
+++ ejercicio1/tbinversorLogico.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//================================================================
+//                          Sys On Chip 
+// 
+// Created by: torjaquintero
+// 
+// Curso de programación en lenguaje Verilog
+// Ejercicio No. 1-test bench
+// El inversor lógico
+// Tarjeta de desarrollo: Nexys A7
+// 
+// Circuito de una puerta lógica inversora NOT
+// 
+//================================================================
+
+module tbpuertaAnd();
+    reg in1,in2;
+    wire outNot;
+    
+    puertaNot ic1(in, out);
+    
+    initial begin
+      $monitor("Time = %t, in = %b, out = %b", $time, in, out);
+    
+        repeat(10) begin
+            in=0;
+            #10;
+        
+            in=1;
+            #10;            
+        end
+        $finish;
+    end
+    
+endmodule
